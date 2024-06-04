385678130881e522075b6d1febcc66750109f5a6
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-06-04 23:21:37 +0200

    v2

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const newJokeButton = document.getElementById('new-joke-button');
 
     newJokeButton.addEventListener('click', async () => {
-        const response = await fetch('/api/jokes/random');
+        const response = await fetch('http://localhost:3000/api/jokes/random');
         const data = await response.json();
         jokeContainer.innerText = data.joke;
     });
